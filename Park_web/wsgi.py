c6aae4df725447971d842898a7ef961b7ce6552a
Ramy <ramitix@ramys-Air.home>
2016-06-29 14:17:39 -0400

    some fixes in the index.html

--- Park_web/wsgi.py
+++ Park_web/wsgi.py
@@ -16,14 +16,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Park_web.settings")
 application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD:Park_web/wsgi.py
+
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-=======
+
 #from whitenoise.django import DjangoWhiteNoise (I comment it to run on windows)
 
 application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)  (I comment it to run on windows)
->>>>>>> f15ff4221db77b8919d387dc6d304c6d1e810213:Park_web/wsgi.py
+
